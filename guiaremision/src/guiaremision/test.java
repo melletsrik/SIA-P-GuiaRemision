ebd3b10630fe89c1e3f8313c25fb733188d8f8cb
Melissa <melkrischf@gmail.com>
2024-06-26 23:58:11 -0500

    p

---
+++ guiaremision/src/guiaremision/test.java
@@ -0,0 +1,34 @@
+
+package guiaremision;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class test {
+    Connection conn = null;
+    String url ="jdbc:postgresql::/192.168.1.100/db_guiaRemision";
+    String usuario="postgres";
+    String clave ="root";
+    
+    public void conectar(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url,usuario,clave);
+            JOptionPane.showMessageDialog(null,"conexion exitosa","conexion",JOptionPane.INFORMATION_MESSAGE);
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null,"conexion fallida" +e,"conexion",JOptionPane.ERROR_MESSAGE); 
+        }
+    }
+    
+    public void cerrar(){
+        try{
+            conn.close();
+            JOptionPane.showMessageDialog(null,"desconexion exitosa","conexion",JOptionPane.INFORMATION_MESSAGE);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,"desconexion fallida" +e,"conexion",JOptionPane.ERROR_MESSAGE); 
+        }
+    }
+    
+  
+}
