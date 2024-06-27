119ce6131998ad3e8e658e2ca15868d6eea9e0a2
melletsrik <142427903+melletsrik@users.noreply.github.com>
2024-06-27 02:32:49 -0500

    Delete guiaremision directory

--- guiaremision/src/guiaremision/arranque.java
+++
@@ -1,9 +0,0 @@
-package guiaremision;
-
-public class arranque {
-    public static void main(String [] args){
-        conn abrir = new conn();
-        abrir.setVisible(true);
-        abrir.setLocationRelativeTo(null);
-    } 
-}
